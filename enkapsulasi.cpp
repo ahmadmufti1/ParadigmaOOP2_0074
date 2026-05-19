1f4c4d023f898407e43326f08a1e6666f2e1a094
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-19 18:54:28 +0700

    membuat function setSaklarNo dengan parameter int i dan string value dan menginisialisi saklarNo[i] ke dalam bentuk value

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -7,5 +7,13 @@ class remoteLampu{
 private:
 //mendeklarasi variabel saklarNo dengan tipe data string dan memiliki nilai 10
 string saklarNo[10];
+public:
+//membuat function setSaklarNo dengan parameter int i dan string value dan menginisialisi saklarNo[i] ke dalam bentuk value
+    void setSaklarNo(int i, string value){
+    saklarNo[i] = value;
+    }
+    //mengambil nilai saklarno dan mengembalikannya
+    string getSaklarNo(int i){
+    return saklarNo[i];
+    }
 };
-
