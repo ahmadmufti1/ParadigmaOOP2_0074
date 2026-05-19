57012a08d894241310c411ae3b754ae4be4feae6
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-19 19:27:18 +0700

    membuat class baseclass dan menggunakan modifikasi final untuk mengunci agar tidak dapat dirubah

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
 using namespace std;
 
+//membuat class baseclass dan menggunakan modifikasi final untuk mengunci agar tidak dapat dirubah
 class baseClass final{
 public:
+    //membuat functio perkenalan
     virtual void perkenalan(){
         cout << "Hallo saya Function dari base class" << endl;
     }
 };
-
-class baseClass1{
-    public:
-    virtual void perkenalan(){
-        cout << "Hallo saya Function dari base class" << endl;
-    }
-};
-
-class derivedClass : public baseClass1{
-    public:
-    void perkenalan(){
-        cout << "Hallo saya Function dari derived class" << endl;
-    }
-};
-
-int main(){
-    derivedClass a;
-    a.perkenalan();
-    return 0;
-}
