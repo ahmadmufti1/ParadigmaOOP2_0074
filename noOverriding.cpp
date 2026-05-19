d62fc027d83c7412715414487c5a24e034f35cf9
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-19 19:14:02 +0700

    membuat class baseClass yang berisi function perkenalan dan mengunci modifikasi

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//membuat class baseClass yang berisi function perkenalan dan mengunci modifikasi
 class baseClass{
     public:
     virtual void perkenalan() final{
