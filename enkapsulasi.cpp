1ac21b8122049cdd1d64d93decab763ba071998b
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-19 18:55:08 +0700

    membuat program utama untuk menajalankan seluruh program

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -17,3 +17,19 @@ public:
     return saklarNo[i];
     }
 };
+
+//membuat program utama untuk menajalankan seluruh program
+int main(){
+    remoteLampu lampu;
+    lampu.setSaklarNo(0, "Lampu Teras Rumah");
+    lampu.setSaklarNo(1, "Lampu Rang Tammu");
+    lampu.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampu.setSaklarNo(3, "Lampu Dapur");
+    
+    cout << lampu.getSaklarNo(0) << endl;
+    cout << lampu.getSaklarNo(1) << endl;
+    cout << lampu.getSaklarNo(2) << endl;
+    cout << lampu.getSaklarNo(3) << endl;
+
+    return 0;
+}
