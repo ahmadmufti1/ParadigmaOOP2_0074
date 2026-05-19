047303f6f8875801e369fd6661856ad896bdd6f8
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-19 18:52:13 +0700

    mendeklarasi variabel saklarNo dengan tipe data string dan memiliki nilai 10

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -4,5 +4,8 @@ using namespace std;
 
 //membuat class remoteLampu
 class remoteLampu{
-
+private:
+//mendeklarasi variabel saklarNo dengan tipe data string dan memiliki nilai 10
+string saklarNo[10];
 };
+
