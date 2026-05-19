052252fe7755acdb124bf0b2863859460d5c2262
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-19 19:30:04 +0700

    membuat class derivedclass dan mengakses baseclass1 untuk menggunakan function perkenalan

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -4,8 +4,30 @@ using namespace std;
 //membuat class baseclass dan menggunakan modifikasi final untuk mengunci agar tidak dapat dirubah
 class baseClass final{
 public:
-    //membuat functio perkenalan
+    //membuat function perkenalan
     virtual void perkenalan(){
         cout << "Hallo saya Function dari base class" << endl;
     }
 };
+
+//membuat class baseclass1 dan menggunakan function dari class baseclass
+class baseClass1{
+    public:
+    virtual void perkenalan(){
+        cout << "Hallo saya Function dari base class" << endl;
+    }
+};
+
+//membuat class derivedclass dan mengakses baseclass1 untuk menggunakan function perkenalan
+class derivedClass : public baseClass1{
+    public:
+    void perkenalan(){
+        cout << "Hallo saya Function dari derived class" << endl;
+    }
+};
+
+int main(){
+    derivedClass a;
+    a.perkenalan();
+    return 0;
+}
