63cd8115936de289668ec4a597f3852c96ead3a1
ahmadmufti1 <muftiahmad617@gmail.com>
2026-05-19 19:07:16 +0700

    membuat class budi dan menggunakan akses public pada class pekerja dan pelajar yang berfungsi untuk memberikan nilai pada budi

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -28,3 +28,21 @@ class pelajar : virtual public orang{
         cout << "pelajar dibuat\n" << endl;
     }
 };
+
+//membuat class budi dan menggunakan akses public pada class pekerja dan pelajar yang berfungsi untuk memberikan nilai pada budi 
+class budi : public pekerja, public pelajar{
+    public:
+    budi(int pUmur) :
+    orang(pUmur),
+    pekerja(pUmur),
+    pelajar(pUmur)
+    {
+        cout << "budi dibuat\n" << endl;
+    }
+};
+
+//membuat program utama untuk menjalankan seluruh program
+int main(){
+    budi a(12);
+    return 0;
+}
